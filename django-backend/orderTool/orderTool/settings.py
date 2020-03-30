b32c484da57ac8d222f403278e1dbfac35178156
JimmyLiao <jimmy5300193@gmail.com>
2020-03-30 05:44:11 -0700

    [Feature] Finish obtain/refresh/verify JWT api 1. Modify requirements.txt for jwt library 2. Modify django setting.py for jwt 3. Add jwt api in url.py

--- django-backend/orderTool/orderTool/settings.py
+++ django-backend/orderTool/orderTool/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backend',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -77,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "order_tool",
-        "HOST": "192.168.56.128",
+        "HOST": "db",
         "PORT": "3306",
         "USER": "root",
         "PASSWORD": "123456"
@@ -122,3 +126,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(seconds=30),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=100000000),
+    'JWT_AUTH_HEADER_PREFIX': "JWT",
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'orderTool.jwt_utils.jwt_response_payload_handler',
+}
+
+###################################
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = ('*')
+
+###################################
--- django-backend/orderTool/orderTool/urls.py
+++ django-backend/orderTool/orderTool/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
-urlpatterns = [
+admin_url = [
     path('admin/', admin.site.urls),
 ]
+
+jwt_token_url = [
+    path('api/token/obtain/', obtain_jwt_token, name='token_obtain_pair'),
+    path('api/token/refresh/', refresh_jwt_token),
+    path('api/token/verify/', verify_jwt_token),
+]
+
+urlpatterns = admin_url + jwt_token_url
